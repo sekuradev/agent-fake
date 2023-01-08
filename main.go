26cc9bed128e7e1c92fa8c190d698faf540c1a6b
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2023-01-08 09:48:49 +0100

    Initial version

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"log"
+	"time"
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	//"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/sekuradev/apigolang/sekuraapi/v1"
+)
+
+var serverName string
+var serverPort int
+var certFile string
+
+func send(){
+
+	serverAddress := fmt.Sprintf("%s:%d", serverName, serverPort)
+
+    creds, err := credentials.NewClientTLSFromFile(certFile, serverName)
+    if err != nil {
+        log.Fatalf("error creating client credentials: %v", err)
+    }
+
+	conn, errConn := grpc.Dial(serverAddress, grpc.WithTransportCredentials(creds))
+	//conn, errConn := grpc.Dial(serverAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if errConn != nil {
+		log.Fatalf("Connection to server could not be stablished at %s: %v", serverAddress, errConn)
+	}
+	defer conn.Close()
+
+	client := pb.NewAgentServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	data := &pb.SetAccessRequest{
+	}
+
+	_, errClient := client.SetAccess(ctx, data)
+	if errClient != nil {
+		log.Printf("error connecting: %v", errClient)
+	}
+}
+
+func main() {
+	flag.StringVar(&serverName, "serverName", "sekura.localhost", "Server address.")
+	flag.IntVar(&serverPort, "serverPort", 50051, "Server port.")
+    flag.StringVar(&certFile, "cert", "", "Server certificate file.")
+    flag.Parse()
+
+	if certFile == "" {
+        log.Fatalln("flag --cert is required")
+    }
+
+	fmt.Printf("Connecting to %s:%d\n", serverName, serverPort)
+	for true{
+		log.Println("Sending data.")
+		send()
+		time.Sleep(10 * time.Second)
+	}
+}
